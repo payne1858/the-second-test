3de94fd06ac5e73af6d518dc11b4667582a887e1
payne <185870317@qq.com>
2019-05-06 16:32:38 +0800

    the first project

---
+++ jmeterdemo1/src/main/java/club/payne/jmeterdemo1/HelloController.java
@@ -0,0 +1,43 @@
+package club.payne.jmeterdemo1;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    public @ResponseBody Object login(){
+
+        ArrayList<Object> list = new ArrayList<>();
+        list.add("tom");
+        list.add("jack");
+        list.add("marry");
+        System.out.println("请求成功...");
+        System.out.println("2秒");
+        return list;
+
+    }
+
+
+    @RequestMapping(value = "/info",method = RequestMethod.POST)
+    public @ResponseBody Object info(String name,String address) {
+        System.out.println("请求成功...");
+        System.out.println("用户姓名是：" + name + "," + "地址是：" + address);
+        ArrayList<Object> list = new ArrayList<>();
+        list.add("tom");
+        list.add(name);
+        return list;
+    }
+
+}
---
+++ jmeterdemo1/src/test/java/club/payne/jmeterdemo1/Jmeterdemo1ApplicationTests.java
@@ -0,0 +1,21 @@
+package club.payne.jmeterdemo1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Jmeterdemo1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+
+
+}
